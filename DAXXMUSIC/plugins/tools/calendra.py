ca5583c5534d0f029993063f8c00a5708fe77c50
𓆩 𝐒˚𝐇˚𝐈˚𝐕˚𝐀˚𝐍˚𝐆 𓆪 <167595776+SHIVANG-BOTS@users.noreply.github.com>
2024-04-23 21:43:25 +0530

    Update calendra.py

--- DAXXMUSIC/plugins/tools/calendra.py
+++ DAXXMUSIC/plugins/tools/calendra.py
@@ -48,5 +48,5 @@ async def send_calendar(_, message):
     carbon_image = await make_carbon(full_year_calendar)
 
     # Send the image as a reply to the user
-    await app.send_photo(message.chat.id, carbon_image, caption=f"✦ ᴄᴀʟᴇɴᴅᴀʀ ᴏғ ᴛʜᴇ ʏᴇᴀʀ {year} ✦\n\n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ๛ɴ ʏ ᴋ ᴀ ᴀ࿐")
+    await app.send_photo(message.chat.id, carbon_image, caption=f"✦ ᴄᴀʟᴇɴᴅᴀʀ ᴏғ ᴛʜᴇ ʏᴇᴀʀ {year} ✦\n\n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ 𝐓ε᧘‌ᴍ|𝐒꯭ιɴꭗ‌")
     
