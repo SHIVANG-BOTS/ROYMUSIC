28fac1cbbd35d13888863142b29ebb38c2365e58
𓆩 𝐒˚𝐇˚𝐈˚𝐕˚𝐀˚𝐍˚𝐆 𓆪 <167595776+SHIVANG-BOTS@users.noreply.github.com>
2024-04-23 21:45:05 +0530

    Update cosplay.py

--- DAXXMUSIC/plugins/tools/cosplay.py
+++ DAXXMUSIC/plugins/tools/cosplay.py
@@ -7,14 +7,14 @@ from config import BOT_USERNAME
 
 NYKAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/nykaaxbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/SinX_Updates"),
     ],
 ]
 
 @app.on_message(filters.command("cosplay"))
 async def cosplay(_,msg):
     img = requests.get("https://waifu-api.vercel.app").json()
-    await msg.reply_photo(img, caption=f"❅ ᴄᴏsᴘʟᴀʏ ʙʏ ➠ ๛ɴ ʏ ᴋ ᴀ ᴀ ༗", reply_markup=InlineKeyboardMarkup(NYKAA),)
+    await msg.reply_photo(img, caption=f"❅ ᴄᴏsᴘʟᴀʏ ʙʏ ➠ 𝐓ε᧘‌ᴍ|𝐒꯭ιɴꭗ‌", reply_markup=InlineKeyboardMarkup(NYKAA),)
 
 
 
@@ -30,4 +30,4 @@ async def ncosplay(_,msg):
     else:
        ncosplay = requests.get("https://waifu-api.vercel.app/items/1").json()
 
-       await msg.reply_photo(ncosplay, caption=f"❅ ᴄᴏsᴘʟᴀʏ ʙʏ ➠ ๛ɴ ʏ ᴋ ᴀ ᴀ ࿐", reply_markup=InlineKeyboardMarkup(NYKAA),)
+       await msg.reply_photo(ncosplay, caption=f"❅ ᴄᴏsᴘʟᴀʏ ʙʏ ➠ 𝐓ε᧘‌ᴍ|𝐒꯭ιɴꭗ‌", reply_markup=InlineKeyboardMarkup(NYKAA),)
